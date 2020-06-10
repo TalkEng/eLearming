4410b61cc407f2d6b1258b82c0147e4220c086f3
Sujal Kumar <sujalvivek123@gmail.com>
2020-06-10 19:42:58 +0530

    second commit

--- learning_e/lib/appModules/achievment_page.dart
+++ learning_e/lib/appModules/achievment_page.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 
 
-/*void main() {
+void main() {
   runApp(MyApp());
-}*/
+}
+
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
